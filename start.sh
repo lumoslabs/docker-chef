86a28fccb55e1454049dff4db1d8437fd591e940
albertrdixon <albert@dixon.rocks>
2017-03-02 16:05:43 -0800

    rev

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 test -n "${_DEBUG}" && set -x
-for d in /mnt/root/usr/local/bin /mnt/root/usr/local/sbin /mnt/root/usr/bin /mnt/root/bin /mnt/root/usr/sbin /mnt/root/sbin; do
+for d in /mnt/root/bin /mnt/root/usr/bin /mnt/root/sbin /mnt/root/usr/sbin /mnt/root/usr/local/bin /mnt/root/usr/local/sbin; do
   test -d "$d" && export PATH=$d:${PATH}
 done
 cmd=$1 ; shift
