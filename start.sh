9626eac04292e4b53ff4d0dbdcab8a73a437518c
albertrdixon <albert@dixon.rocks>
2017-03-02 16:02:29 -0800

    duh

--- start.sh
+++ start.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 test -n "${_DEBUG}" && set -x
-dirs=(
-  /mnt/root/usr/local/bin
-  /mnt/root/usr/local/sbin
-  /mnt/root/usr/bin
-  /mnt/root/bin
-  /mnt/root/usr/sbin
-  /mnt/root/sbin
-)
-for d in "${dirs[@]}"; do
+for d in /mnt/root/usr/local/bin /mnt/root/usr/local/sbin /mnt/root/usr/bin /mnt/root/bin /mnt/root/usr/sbin /mnt/root/sbin; do
   test -d "$d" && export PATH=$d:${PATH}
 done
 cmd=$1 ; shift
